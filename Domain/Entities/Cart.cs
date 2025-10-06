1065a3d5427c6e39d9900756746c9978a594b7e6
kochkaqqq <megakochka4g@gmail.com>
2025-10-06 16:39:20 +0500

    add domain entities

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+	public class Cart
+	{
+		public int CartId { get; set; }
+		public User User { get; set; } = null!;
+		public ICollection<Nomenclature> Nomenclatures { get; set; } = null!;
+		public decimal Amount { get; set; }
+	}
+}
